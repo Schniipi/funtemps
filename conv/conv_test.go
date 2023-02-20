8eea316ea5ea3ee2ba69da3adddd1309224a8fdf
Oliver Aaron Berg Johnston <oliveraaron03@gmail.com>
2023-02-20 20:10:31 +0100

    Endret på conv_test.go

--- conv/conv_test.go
+++ conv/conv_test.go
@@ -1,6 +1,7 @@
 package conv
 
 import (
+	"math"
 	"reflect"
 	"testing"
 )
@@ -22,12 +23,149 @@ func TestFarhenheitToCelsius(t *testing.T) {
 
 	tests := []test{
 		{input: 134, want: 56.67},
+		{input: 32, want: 0},
 	}
 
 	for _, tc := range tests {
-		got := FarhenheitToCelsius(tc.input)
-		if !reflect.DeepEqual(tc.want, got) {
-			t.Errorf("expected: %.2f, got: %.2f", tc.want, got)
+		got := FahrenheitToCelsius(tc.input)
+		if math.Floor(got) == got {
+			// Hvis "got" er et heltall formater uten desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.0f, got: %.0f", tc.want, got)
+			}
+		} else {
+			// Hvis "got" ikke er et heltall så formaterer den med 2 desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.2f, got: %.2f", tc.want, got)
+			}
+		}
+	}
+}
+func TestFahrenheitToKelvin(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 0, want: 255.37},
+	}
+
+	for _, tc := range tests {
+		got := FahrenheitToKelvin(tc.input)
+		if math.Floor(got) == got {
+			// Hvis "got" er et heltall formater uten desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.0f, got: %.0f", tc.want, got)
+			}
+		} else {
+			// Hvis "got" ikke er et heltall så formaterer den med 2 desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.2f, got: %.2f", tc.want, got)
+			}
+		}
+	}
+}
+func TestCelsiusToFahrenheit(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 89.4, want: 128.92},
+	}
+
+	for _, tc := range tests {
+		got := CelsiusToFahrenheit(tc.input)
+		if math.Floor(got) == got {
+			// Hvis "got" er et heltall formater uten desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.0f, got: %.0f", tc.want, got)
+			}
+		} else {
+			// Hvis "got" ikke er et heltall så formaterer den med 2 desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.2f, got: %.2f", tc.want, got)
+			}
+		}
+	}
+}
+
+func TestCelsiusToKelvin(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 89.4, want: 183.75},
+	}
+
+	for _, tc := range tests {
+		got := CelsiusToKelvin(tc.input)
+		if math.Floor(got) == got {
+			// Hvis "got" er et heltall formater uten desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.0f, got: %.0f", tc.want, got)
+			}
+		} else {
+			// Hvis "got" ikke er et heltall så formaterer den med 2 desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.2f, got: %.2f", tc.want, got)
+			}
+		}
+	}
+}
+
+func TestKelvinToCelsius(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 273.15, want: 0},
+	}
+
+	for _, tc := range tests {
+		got := KelvinToCelsius(tc.input)
+		if math.Floor(got) == got {
+			// Hvis "got" er et heltall formater uten desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.0f, got: %.0f", tc.want, got)
+			}
+		} else {
+			// Hvis "got" ikke er et heltall så formaterer den med 2 desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.2f, got: %.2f", tc.want, got)
+			}
+		}
+	}
+}
+
+func TestKelvinToFahrenheit(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 300, want: 80.33},
+	}
+
+	for _, tc := range tests {
+		got := KelvinToFahrenheit(tc.input)
+		if math.Floor(got) == got {
+			// Hvis "got" er et heltall formater uten desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.0f, got: %.0f", tc.want, got)
+			}
+		} else {
+			// Hvis "got" ikke er et heltall så formaterer den med 2 desimaler
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("expected: %.2f, got: %.2f", tc.want, got)
+			}
 		}
 	}
 }
